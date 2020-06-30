0f95355089aac167c66b8cb0335ed13e1ca0098f
Kartikeya Chauhan <kartikeyasingh76@gmail.com>
2020-06-30 14:24:21 +0530

    Added Error modules

--- log-actions.ps1
+++ log-actions.ps1
@@ -8,7 +8,7 @@
 
 
 # Function 1 - Delete-Log   - delete log older than 7 days
-
+# Usage - Delete-Logs -Directory "path to logs"
 
 function Delete-Logs {
     param (
@@ -24,3 +24,39 @@ function Delete-Logs {
         $_.FullName | Out-File .\deletelog.txt -Append
     }
 }
+
+# Another function Find-Error
+# Usage - Find-Error - Directory "Path to logs"
+function Find-Error {
+    param (
+        # Parameter help description
+        [Parameter()]
+        [string] $Directory
+    )
+    $fileList = Get-ChildItem $Directory
+    $currentLocation = Get-Location
+
+    Set-Location $Directory
+
+    foreach ($file in $fileList){
+        Write-Output "-----Checking in file $file"
+        $content = Get-Content $file
+
+        $count = 1
+
+        foreach ($line in $content) {
+            if ( $line | Select-String -Pattern "Error") {
+                Write-Error ">> Error at line $count "
+                Write-Error "$line"
+            }
+            $count = $count+1
+        }
+    }
+    Set-Location $currentLocation
+}
+
+# Function Call
+
+Delete-Logs -Directory "d:\ds\3ds\winb_64\tomee\logs"
+
+Find-Error -Directory "d:\ds\3ds\winb_64\tomee\logs"
